1c861f396c444a6fd7499cfd65729ae459c45631
janivn <202080160@psu.palawan.edu.ph>
2023-12-12 01:58:22 +0800

    Done

---
+++ cardquest/cardquest/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+# Register models in admin.py
+from .models import PokemonCard
+from .models import Trainer
+from .models import Collection
+
+@admin.register(PokemonCard)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ("name", "rarity", "hp", "card_type", "attack", "description", "weakness", "card_number", "release_date", "evolution_stage", "abilities")
+    search_fields = ("name", "rarity", "hp", "card_type", "attack", "description", "weakness", "card_number", "release_date", "evolution_stage", "abilities")
+
+@admin.register(Trainer)
+class TrainerAdmin(admin.ModelAdmin):
+    list_display = ("name", "birthdate", "location", "email")
+    search_fields = ("name", "birthdate", "location", "email")
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ("card", "trainer", "collection_date")
+    search_fields = ("card", "trainer", "collection_date")
